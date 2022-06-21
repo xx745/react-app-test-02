928ade1ca0a35c012de29e2469db2f4aee867714
Byron Morley <Byron.Morley@catalyst-it.co.uk>
2022-06-21 15:01:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MyAlert from './MyAlert';
+import {Stack, Typography} from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            warning: false,
+            info   : false,
+        }
+    }
+
+    componentDidMount() {
+        var info = true;
+        this.setState({warning: info})
+    }
+
+    render() {
+        const {info} = this.state;
+
+        return (
+            <>
+                <Stack sx={{width: '600px', padding: '20px'}} spacing={2}>
+                    <Typography variant={'h4'}>Alerts</Typography>
+                    {
+                        this.state.warning ? <MyAlert
+                            type={'warning'}
+                            title={'Warning'}
+                            content={'This is a warning message'}
+                        /> :null
+                    }
+                    {
+                        info ? <MyAlert
+                            type={'info'}
+                            title={'Information'}
+                            content={'This is a information message'}
+                        /> :null
+                    }
+                </Stack>
+            </>
+        );
+    }
+};
 
 export default App;
---
+++ src/MyAlert.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
+const MyAlert = ({type, title, content}) => {
+
+    return (
+        <Alert severity={type}>
+            <AlertTitle>title</AlertTitle>
+            content
+        </Alert>
+    );
+};
+
+export default MyAlert;
